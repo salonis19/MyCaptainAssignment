3fb82cd56b086d281bee3da3fa83e3bd75505e05
salonis19 <90784749+salonis19@users.noreply.github.com>
2021-10-21 19:36:01 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from math import pi
+
+r = float(input ("Enter radius of circle : "))
+
+print ("Area of the circle is: " + str(pi * r**2))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
